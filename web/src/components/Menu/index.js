e790a5e5f77ed0e5a414904103eedea57198de09
Carol <51242985+carolquiterio@users.noreply.github.com>
2020-09-17 02:15:55 -0300

    home page

--- web/src/components/Menu/index.js
+++ web/src/components/Menu/index.js
@@ -7,40 +7,37 @@ import backHomeImage from "../../assets/page.svg";
 export default class Menu extends Component {
   render() {
     return (
-      <div className="fundo">
-        <div className="menu-container">
-          <a className="itemMenu" href="/">
-            Contato
-          </a>
-          <a className="itemMenu" href="/">
-            Clientes
-          </a>
-          <a className="itemMenu" href="/">
-            Trabalhos
-          </a>
-          <a className="itemMenu" href="/">
-            Serviços
-          </a>
-          <a className="itemMenu" href="/">
-            Sobre
-          </a>
-          <a className="itemMenu" href="/">
-            Início
-          </a>
-        </div>
-        <div className={this.props.className}>
-          <div className="small-div"></div>
-
-          <div className="big-div">
-            <span className="div-title">Bem vindo(a) a</span>
-            <br />
-            <span className="div-title-bold">Samambaia Tech</span>
-
-            <br />
-            <span className="description">{this.props.description}</span>
+      <div id="page-landing">
+        <div id="page-landing-content" className="container">
+          <div className="menu-container">
+            <a className="itemMenu" href="/">
+              Contato
+            </a>
+            <a className="itemMenu" href="/">
+              Clientes
+            </a>
+            <a className="itemMenu" href="/">
+              Trabalhos
+            </a>
+            <a className="itemMenu" href="/">
+              Serviços
+            </a>
+            <a className="itemMenu" href="/">
+              Sobre
+            </a>
+            <a className="itemMenu" href="/">
+              Início
+            </a>
           </div>
+          <div>
+            <div className="logo-container">
+              <h1>Bem vindo(a) a</h1>
+              <h2>Samambaia Tech</h2>
+              <h5 className="description">{this.props.description}</h5>
+            </div>
 
-          <button className="start-button">Entrar em contato</button>
+            <button className="start-button">Entrar em contato</button>
+          </div>
         </div>
       </div>
     );
