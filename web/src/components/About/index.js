1d6f5f9c5b6a06e359ba5517e48c24aff8e2dc21
Carol <51242985+carolquiterio@users.noreply.github.com>
2020-09-20 01:55:13 -0300

    about page structure

--- web/src/components/About/index.js
+++ web/src/components/About/index.js
@@ -1,20 +1,27 @@
 import React, { Component } from "react";
 import "./styles.css";
+import pcImage from "../../assets/pcs.svg";
 
 export default class About extends Component {
   render() {
     return (
       <div id="page-landing-about">
-        <div id="page-landing-content" className="container">
-          <div>
-            <div className="logo-container">
-              <h1>Bem vindo(a) a</h1>
-              <h2>Samambaia Tech</h2>
-              <h5 className="description">{this.props.description}</h5>
-            </div>
-            <div className="button-container">
-              <button className="start-button">Entrar em contato</button>
-            </div>
+        <div id="page-landing-content-about" className="container">
+          <div></div>
+          <div className="text-about">
+            <h1>SOBRE NÓS</h1> <br />
+            <h4>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </h4>
+            <br />
+            <h3>Porque escolher?</h3>
+            <ul>
+              <li>osfsdfdi</li>
+              <li>odfsdfi</li>
+            </ul>
           </div>
         </div>
       </div>
