1dabe6f399d8991878402fa28f5242418303266e
Carol <51242985+carolquiterio@users.noreply.github.com>
2020-09-28 10:36:36 -0300

    contact ok

--- web/src/components/Contact/index.js
+++ web/src/components/Contact/index.js
@@ -2,14 +2,16 @@ import React, { Component } from "react";
 
 import pcImage from "../../assets/pcs.svg";
 
+import "./styles.css";
+
 export default class Contact extends Component {
   render() {
     return (
-      <div id="contact" className="page-landing">
-        <div id="page-landing-content-about" className="container">
+      <div id="contact" className="page-landing-contact">
+        <div id="page-landing-content-contact" className="container-contact">
           <div></div>
-          <div className="text-about">
-            <h1>Contate-nos</h1> <br />
+          <div className="text-contact">
+            <h1>ENTRE EM CONTATO</h1> <br />
             <h4>
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
@@ -17,11 +19,32 @@ export default class Contact extends Component {
               type and scrambled it to make a type specimen book.
             </h4>
             <br />
-            <h3>Porque escolher?</h3>
-            <ul>
-              <li>osfsdfdi</li>
-              <li>odfsdfi</li>
-            </ul>
+            <form className="form-contact">
+              <input
+                type="text"
+                name="name"
+                className="contact-input"
+                placeholder="Seu nome"
+              />
+              <input
+                type="text"
+                name="name"
+                className="contact-input"
+                placeholder="Seu email"
+              />
+              <input
+                type="text"
+                name="name"
+                className="contact-input"
+                placeholder="Sua mensagem"
+              />
+              <br />
+              <input
+                type="submit"
+                value="Enviar"
+                className="form-button-contact"
+              />
+            </form>
           </div>
         </div>
       </div>
